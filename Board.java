b662dc22ca36f50377db6a76e8a3c0a2bb76092b
Student: Hussein Hussein <94848869+Student-Hussein-Baiz@users.noreply.github.com>
2022-03-10 14:30:55 +0000

    Added constructor and toString

--- Board.java
+++ Board.java
@@ -3,11 +3,35 @@ public class Board{
   private String[][] squares;
 
   public Board(){
-
+squares = new String[10][10];
+for(int r = 0; r < squares.length; r++){
+for(int c = 0; c <squares[0].length; c++){
+  squares[r][c] = "-";
+}}
   }
 
   public String toString(){
-    return "";
+
+String ret = "";
+    int counter = 0;
+    
+for(int r = 0; r < squares.length; r++){
+  
+for(int c = 0; c <squares[0].length; c++){
+
+  if(counter == 10){
+ret += "\n" + (squares[r][c]) + " ";
+    counter = 0;
+  }else{
+ret += (squares[r][c]) + " ";
+    }
+  counter++;
+  
+  }
+
+  }
+    
+    return ret;
   }
 
   public boolean addShip(int row, int col, int len, boolean horizontal){
